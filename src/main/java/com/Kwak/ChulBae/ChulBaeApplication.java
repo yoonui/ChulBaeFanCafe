c2254c146cedd471c1fac75875e8119dbcb4f030
yoonui <yoony0612@ajou.ac.kr>
2022-05-30 18:07:22 +0900

    프로젝트 시작

---
+++ src/main/java/com/Kwak/ChulBae/ChulBaeApplication.java
@@ -0,0 +1,13 @@
+package com.Kwak.ChulBae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChulBaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChulBaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kwak/ChulBae/ChulBaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kwak.ChulBae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChulBaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
